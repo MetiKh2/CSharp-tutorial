755bac3127b65a82983763b480b3b3952a4bbf10
MetiKh2 <90332147+MetiKh2@users.noreply.github.com>
2022-05-26 13:10:05 +0430

    Create CSharp-Comments.cs

---
+++ CSharp-Comments.cs
@@ -0,0 +1,17 @@
+//Single-line Comments
+
+// This is a comment
+Console.WriteLine("Hello world");
+
+Console.WriteLine("Hello World"); //this is a comment
+
+//Multi-line Comments
+
+/* The code below will print the 
+ words hello world to the 
+ screen ,and it amazing
+ */
+Console.WriteLine("Hello world");
+
+
+
