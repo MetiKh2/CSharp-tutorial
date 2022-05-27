c9405474be5e0b263aa10aa8fe77913521c2e25d
MetiKh2 <90332147+MetiKh2@users.noreply.github.com>
2022-05-27 12:20:36 +0430

    Create CSharp-Variables.cs

---
+++ CSharp-Variables.cs
@@ -0,0 +1,32 @@
+//string name = "John";
+//Console.WriteLine(name);
+
+//int myNum = 15;
+//myNum = 20;
+//Console.WriteLine(myNum);
+
+//const int myNum2=10;
+////myNum2 = 20; //error
+//Console.WriteLine(myNum2);
+
+//int myNum3=10;
+//double myDoubleNum = 4.89;
+//char myLetter = 'D';
+//bool myBool = false;
+//string myText = "Hello";
+
+string name = "John ";
+string lastName = "Doe";
+string fullName = name + lastName;
+Console.WriteLine(fullName);
+
+//int x = 4;
+//int y = 5;
+//Console.WriteLine(x+y);
+
+//int x=5, y=6,z=10;
+//Console.WriteLine(x+y+z);
+
+int x, y, z;
+x = y = z = 10;
+Console.WriteLine(x+y+z);
