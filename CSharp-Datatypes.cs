28abfc7578acb52d328589fcf40e9b54a57174a9
MetiKh2 <90332147+MetiKh2@users.noreply.github.com>
2022-05-28 20:58:55 +0430

    Create CSharp-Datatypes.cs

---
+++ CSharp-Datatypes.cs
@@ -0,0 +1,39 @@
+//Integer Types
+int myNum = 11;
+Console.WriteLine(myNum);
+Console.WriteLine(int.MinValue);
+Console.WriteLine(int.MaxValue);
+
+long myLong = 100;
+Console.WriteLine(myLong);
+
+Console.WriteLine(long.MinValue);
+Console.WriteLine(long.MaxValue);
+//Floating Point Types
+double myDouble = 10.10;
+float myFloat = 8.9F;
+Console.WriteLine(myDouble);
+Console.WriteLine(myFloat);
+
+Console.WriteLine(float.MinValue);
+Console.WriteLine(float.MaxValue);
+
+Console.WriteLine(double.MinValue);
+Console.WriteLine(double.MaxValue);
+//Booleans
+bool isCSharpFun = true;
+bool isFishTasty = false;
+
+Console.WriteLine(isCSharpFun);
+Console.WriteLine(isFishTasty);
+
+//Characters
+char myChar1 = 'A';
+char myChar2 = 'b';
+Console.WriteLine(myChar1);
+Console.WriteLine(myChar2);
+
+//Strings
+
+string name = "Mahdi";
+Console.WriteLine(name);
