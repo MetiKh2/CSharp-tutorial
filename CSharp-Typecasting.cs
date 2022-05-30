792df404fe04e4fce7a25f9ade3d64a77a6bf5d7
MetiKh2 <90332147+MetiKh2@users.noreply.github.com>
2022-05-30 20:11:08 +0430

    Create CSharp-Typecasting.cs

---
+++ CSharp-Typecasting.cs
@@ -0,0 +1,30 @@
+//Implicit Casting 
+
+//int myInt = 9;
+//double myDouble = myInt;
+
+//Console.WriteLine(myDouble);
+//Console.WriteLine(myInt);
+
+//Explicit Casting
+
+//double myDouble = 9.78;
+//int myInt = (int)myDouble;
+
+//Console.WriteLine(myInt);
+//Console.WriteLine(myDouble);
+
+//Type Conversion Methods
+
+int myInt = 10;
+double myDouble = 5.25;
+bool myBool = false;
+
+Console.WriteLine(Convert.ToString(myInt)); //conevrt int to string
+
+Console.WriteLine(Convert.ToDouble(myInt)); //convert int to double
+
+Console.WriteLine(Convert.ToInt32(myDouble)); //convert double to int
+
+Console.WriteLine(Convert.ToString(myBool)); //convert bool to string
+ 
