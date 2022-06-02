d2d104bbe2dd894e4539f3840b03cca98b638aca
MetiKh2 <90332147+MetiKh2@users.noreply.github.com>
2022-06-02 11:10:29 +0430

    Create CSharp-Math.cs

---
+++ CSharp-Math.cs
@@ -0,0 +1,10 @@
+//Math.Max(x,y)
+Console.WriteLine(Math.Max(6, 16));
+//Math.Min(x,y)
+Console.WriteLine(Math.Min(6, 16));
+//Math.Sqrt(x)
+Console.WriteLine(Math.Sqrt(64));
+//Math.Abs(x)
+Console.WriteLine(Math.Abs(-3));
+//Math.Round(x)
+Console.WriteLine(Math.Round(9.8));
