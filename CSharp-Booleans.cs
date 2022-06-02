a9eafdd3ec84d6fd46d4263025ea5b7f6ffef53c
MetiKh2 <90332147+MetiKh2@users.noreply.github.com>
2022-06-02 12:55:47 +0430

    Create CSharp-Booleans.cs

---
+++ CSharp-Booleans.cs
@@ -0,0 +1,12 @@
+//Boolean Values
+bool isProrammingGood = true;
+bool isProrammingBad = false;
+Console.WriteLine(isProrammingGood);
+Console.WriteLine(isProrammingBad);
+//Boolean Expression
+int x = 10;
+int y = 8;
+Console.WriteLine(x>y);
+Console.WriteLine(100<10);
+Console.WriteLine(x==10);
+Console.WriteLine(x==1);
