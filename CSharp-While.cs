9463f8586af7c7e40aca722a7e418b3428ab2157
MetiKh2 <90332147+MetiKh2@users.noreply.github.com>
2022-06-05 14:24:51 +0430

    Create CSharp-While.cs

---
+++ CSharp-While.cs
@@ -0,0 +1,30 @@
+//While Loop
+//Syntax 
+/*
+ while(condition)
+{
+
+}
+ */
+int i = 0;
+while (i < 5)
+{
+    Console.WriteLine(i);
+    i++;
+}
+//i++ == i=i+1;
+// Do/While Loop
+//Syntax
+/*
+ do 
+{
+
+}while(condition)
+ */
+Console.WriteLine("_________________________________________");
+int k = 1;
+do
+{
+    Console.WriteLine(k);
+    k++;
+}while (k<0);
