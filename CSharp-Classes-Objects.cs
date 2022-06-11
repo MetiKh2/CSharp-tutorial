115d31e6aa6ad9596c839b9ae79711115f05e69f
MetiKh2 <90332147+MetiKh2@users.noreply.github.com>
2022-06-12 03:32:58 +0430

    Create CSharp-Classes-Objects.cs

---
+++ CSharp-Classes-Objects.cs
@@ -0,0 +1,11 @@
+//Create an Object
+Car myObj = new Car();
+Car myObj2 = new Car();
+Console.WriteLine(myObj.color);
+Console.WriteLine(myObj2.color);
+
+//Create a Class
+class Car
+{
+    public string color = "red";
+}
